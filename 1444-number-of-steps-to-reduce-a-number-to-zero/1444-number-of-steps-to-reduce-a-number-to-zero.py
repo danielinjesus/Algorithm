1fa3d9e15368d4c088fc38cdf0e46d800462b34b
Azure Lover <cmsg_hbg2@naver.com>
2023-10-07 13:20:58 +0900

    Time: 7 ms (96.67%), Space: 13.3 MB (16.1%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.py
@@ -1,10 +1,10 @@
 class Solution(object):
     def numberOfSteps(self, num):
-        k = 0
+        k=0
         while num!=0:
-            if num % 2 == 0:
-                num = num / 2                
+            if num%2==0:
+                num=num/2                
             else:
-                num -= 1
-            k += 1
+                num-=1
+            k+=1
         return k
