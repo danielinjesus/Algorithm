7f576fc9aafb70807a5e71e096c667968680b7e0
Azure Lover <cmsg_hbg2@naver.com>
2023-10-07 13:12:42 +0900

    Time: 19 ms (15.8%), Space: 13.4 MB (16.1%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.py
@@ -5,11 +5,9 @@ class Solution(object):
             if num == 0:
                 break
             if num % 2 == 0:
-                num = num / 2
+                num /= 2
                 step += 1
             if num % 2 == 1:
                 num -= 1
                 step += 1
-            if num == 0:
-                break
         return(step)
